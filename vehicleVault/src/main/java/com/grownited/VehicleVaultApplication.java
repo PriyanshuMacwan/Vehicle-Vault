2fead0c35893c61918a71e86b3cc0f0fdd353c40
PriyanshuMacwan <priyanshumacwan2003@gmail.com>
2025-02-07 16:33:51 +0530

    init

---
+++ vehicleVault/src/main/java/com/grownited/VehicleVaultApplication.java
@@ -0,0 +1,13 @@
+package com.grownited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleVaultApplication.class, args);
+	}
+
+}
